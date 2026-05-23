13465a423765e6b427267fa348930568740b1beb
irinaradanovic <irina.radanovic5@gmail.com>
2026-05-23 12:15:01 +0200

    Initial Spring Boot setup

---
+++ src/main/java/fooddelivery/food_delivery_platform/FoodDeliveryPlatformApplication.java
@@ -0,0 +1,13 @@
+package fooddelivery.food_delivery_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliveryPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fooddelivery/food_delivery_platform/FoodDeliveryPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package fooddelivery.food_delivery_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
