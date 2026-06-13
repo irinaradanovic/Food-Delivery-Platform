58902dbf4a66582de1a437e6b92a6f803cf53e9e
MarkoDjurdjevic03 <markodjurdjevic03@gmail.com>
2026-06-13 20:43:09 +0200

    Prilagodjavanje sa vremenskim menijem

--- src/main/java/fooddelivery/food_delivery_platform/repository/MeniRepository.java
+++ src/main/java/fooddelivery/food_delivery_platform/repository/MeniRepository.java
@@ -2,11 +2,21 @@ package fooddelivery.food_delivery_platform.repository;
 
 import fooddelivery.food_delivery_platform.model.Meni;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalTime;
 import java.util.List;
 
 public interface MeniRepository extends JpaRepository<Meni, Long> {
     List<Meni> findByRestoranRestoranId(Long restoranId);
 
     List<Meni> findByRestoranRestoranIdAndAktivanTrue(Long restoranId);
+
+
+    @Query("SELECT m FROM Meni m WHERE m.restoran.restoranId = :restoranId AND m.aktivan = true " +
+            "AND (m.vremeOd IS NULL OR (m.vremeOd <= :trenutnoVreme AND m.vremeDo >= :trenutnoVreme))")
+    List<Meni> findAktivniZaKupcaSaVremenskimFilterom(
+            @Param("restoranId") Long restoranId,
+            @Param("trenutnoVreme") LocalTime trenutnoVreme);
 }
--- src/main/java/fooddelivery/food_delivery_platform/service/MeniService.java
+++ src/main/java/fooddelivery/food_delivery_platform/service/MeniService.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 import java.util.List;
 
@@ -34,7 +35,7 @@ public class MeniService {
     }
 
     public List<Meni> findAktivniByRestoranRestoranId(Long restoranId) {
-        return meniRepository.findByRestoranRestoranIdAndAktivanTrue(restoranId);
+        return meniRepository.findAktivniZaKupcaSaVremenskimFilterom(restoranId, LocalTime.now());
     }
 
     public Meni getById(Long id) {
